cb8057ecdcc7204d5dab6b0a8f8e5cb98f8b7724
Michal Špaček <tupinek@gmail.com>
2014-08-02 18:28:22 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+
+Scrape data about [Aktuální usnesení rady](http://www.sever.brno.cz/volene-organy-s/646-aktualni-usneseni-rady.html)
---
+++ scraper.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/env perl
+# Copyright 2014 Michal Špaček <tupinek@gmail.com>
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Database::DumpTruck;
+use Encode qw(decode_utf8 encode_utf8);
+use English;
+use HTML::TreeBuilder;
+use LWP::UserAgent;
+use POSIX qw(strftime);
+use URI;
+use Time::Local;
+
+# Don't buffer.
+$OUTPUT_AUTOFLUSH = 1;
+
+# URI of service.
+my $base_uri = URI->new('http://www.sever.brno.cz/volene-organy-s/646-aktualni-usneseni-rady.html');
+
+# Open a database handle.
+my $dt = Database::DumpTruck->new({
+	'dbname' => 'data.sqlite',
+	'table' => 'data',
+});
+
+# Create a user agent object.
+my $ua = LWP::UserAgent->new(
+	'agent' => 'Mozilla/5.0',
+);
+
+# Get base root.
+print 'Page: '.$base_uri->as_string."\n";
+my $root = get_root($base_uri);
+
+# Look for items.
+my $act_year = (localtime)[5] + 1900;
+foreach my $year (2010 .. $act_year) {
+	print "$year\n";
+	my $year_div = get_h3_content($year);
+	my @tr = $year_div->find_by_tag_name('tr');
+	foreach my $tr (@tr) {
+		my ($date_td, $doc_td) = $tr->find_by_tag_name('td');
+		my $date = get_db_date($date_td->as_text);
+		my $doc_a = $doc_td->find_by_tag_name('a');
+
+		# Save.
+		if ($doc_a) {
+			my $title = $doc_td->as_text;
+			remove_trailing(\$title);
+			my $link = $base_uri->scheme.'://'.$base_uri->host.
+				$doc_a->attr('href');
+			my $rmc_number;
+			my $rmc = decode_utf8('RMČ');
+			if ($title =~ m/^\s*(\d+)\.\s*$rmc$/ms) {
+				$rmc_number = $1;
+			}
+
+			# Save.
+			# TODO Update.
+			print '- '.encode_utf8($title)."\n";
+			$dt->insert({
+				'Date' => $date,
+				'Title' => $title,
+				'PDF_link' => $link,
+				'RMC_number' => $rmc_number,
+			});
+			# TODO Move to begin with create_table().
+			$dt->create_index(['Date', 'Title'], 'data', 1, 1);
+		}
+	}
+}
+
+# Get database date from web date.
+sub get_db_date {
+	my $web_date = shift;
+	my ($day, $mon, $year) = $web_date
+		=~ m/^\s*(\d+)\s*\.\s*(\d+)\s*.\s*(\d+)\s*$/ms;
+	my $time = timelocal(0, 0, 0, $day, $mon - 1, $year - 1900);
+	return strftime('%Y-%m-%d', localtime($time));
+}
+
+# Get content after h3 defined by title.
+sub get_h3_content {
+	my $title = shift;
+	my @a = $root->find_by_tag_name('a');
+	my $ret_a;
+	foreach my $a (@a) {
+		if ($a->as_text eq $title) {
+			$ret_a = $a;
+			last;
+		}
+	}
+	my @content = $ret_a->parent->parent->content_list;
+	my $num = 0;
+	foreach my $content (@content) {
+		if ($num == 1) {
+			return $content;
+		}
+		if (check_h3($content, $title)) {
+			$num = 1;
+		}
+	}
+	return;
+}
+
+# Check if is h3 with defined title.
+sub check_h3 {
+	my ($block, $title) = @_;
+	foreach my $a ($block->find_by_tag_name('a')) {
+		if ($a->as_text eq $title) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+# Get root of HTML::TreeBuilder object.
+sub get_root {
+	my $uri = shift;
+	my $get = $ua->get($uri->as_string);
+	my $data;
+	if ($get->is_success) {
+		$data = $get->content;
+	} else {
+		die "Cannot GET '".$uri->as_string." page.";
+	}
+	my $tree = HTML::TreeBuilder->new;
+	$tree->parse(decode_utf8($data));
+	return $tree->elementify;
+}
+
+# Removing trailing whitespace.
+sub remove_trailing {
+	my $string_sr = shift;
+	${$string_sr} =~ s/^\s*//ms;
+	${$string_sr} =~ s/\s*$//ms;
+	return;
+}
